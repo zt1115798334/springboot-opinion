b6dfc09040a6677665921e7d31a7cb450ea357fb
zhangtong <zhangtong9498@qq.com>
2017-07-04 14:56:35 +0800

    message

--- src/test/java/com/zt/opinion/jsoup/JsoupTest.java
+++ src/test/java/com/zt/opinion/jsoup/JsoupTest.java
@@ -1,11 +1,15 @@
 package com.zt.opinion.jsoup;
 
+import com.google.common.collect.Collections2;
+import com.zt.opinion.entity.Group;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * Created by zhangtong on 2017/7/4.
@@ -32,5 +36,13 @@ public class JsoupTest {
         Document doc = Jsoup.connect("http://www.baidu.com").get();
         String title = doc.title();
         System.out.println(title);
+    }
+
+    @Test
+    public void test4(){
+        Group group = new Group();
+        System.out.println(group==null);
+        System.out.println(StringUtils.isNotBlank(" "));
+        System.out.println(StringUtils.isNotEmpty(" "));
     }
 }
