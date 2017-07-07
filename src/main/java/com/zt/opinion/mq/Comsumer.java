af9d9b7188bacf78f942cec8a537da2cf4344e00
zhangtong <zhangtong9498@qq.com>
2017-07-07 09:58:05 +0800

    message

--- src/main/java/com/zt/opinion/mq/Comsumer.java
+++ src/main/java/com/zt/opinion/mq/Comsumer.java
@@ -15,46 +15,46 @@ public class Comsumer {
     private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
 
     private static final String BROKEN_URL = ActiveMQConnection.DEFAULT_BROKER_URL;
-//    private static final String BROKEN_URL = "failover://tcp://localhost:61616";
-    ConnectionFactory connectionFactory;
+    //    private static final String BROKEN_URL = "failover://tcp://localhost:61616";
+    private ConnectionFactory connectionFactory;
 
-    Connection connection;
+    private Connection connection;
 
-    Session session;
+    private Session session;
 
-    ThreadLocal<MessageConsumer> threadLocal = new ThreadLocal<>();
-    AtomicInteger count = new AtomicInteger();
+    private ThreadLocal<MessageConsumer> threadLocal = new ThreadLocal<>();
+    private  AtomicInteger count = new AtomicInteger();
 
-    public void init(){
+    public void init() {
         try {
-            connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEN_URL);
-            connection  = connectionFactory.createConnection();
+            connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKEN_URL);
+            connection = connectionFactory.createConnection();
             connection.start();
-            session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         } catch (JMSException e) {
             e.printStackTrace();
         }
     }
 
 
-    public void getMessage(String disname){
+    public void getMessage(String disname) {
         try {
             Queue queue = session.createQueue(disname);
             MessageConsumer consumer = null;
 
-            if(threadLocal.get()!=null){
+            if (threadLocal.get() != null) {
                 consumer = threadLocal.get();
-            }else{
+            } else {
                 consumer = session.createConsumer(queue);
                 threadLocal.set(consumer);
             }
-                TextMessage msg = (TextMessage) consumer.receive();
-                if(msg!=null) {
-                    msg.acknowledge();
-                    System.out.println(Thread.currentThread().getName()+": Consumer:我是消费者，我正在消费Msg"+msg.getText()+"--->"+count.getAndIncrement());
-                }else {
-                    return;
-                }
+            TextMessage msg = (TextMessage) consumer.receive();
+            if (msg != null) {
+                msg.acknowledge();
+                System.out.println(Thread.currentThread().getName() + ": Consumer:我是消费者，我正在消费Msg" + msg.getText() + "--->" + count.getAndIncrement());
+            } else {
+                return;
+            }
         } catch (JMSException e) {
             e.printStackTrace();
         }
